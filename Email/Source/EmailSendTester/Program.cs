cf638239acf99330941a5b25287328ab2a4c0167
Haneex <62711547+Haneex@users.noreply.github.com>
2020-05-27 19:49:14 +0300

    Initial code
    First commit

---
+++ Email/Source/EmailSendTester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmailSendTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
